dbdc664f933d287909be9aa9170b8f66ef7097ca
Shaun <shaun@thenetninja.co.uk>
2020-09-15 18:02:18 +0100

    lesson-1

---
+++ src/components/Articles.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Articles = () => {
+  return (
+    <div className="articles">
+      <h2>All Articles</h2>
+    </div>
+  )
+}
+
+export default Articles
---
+++ src/components/User.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const User = () => {
+  return (
+    <div className="user">
+      <h2>User Details</h2>
+    </div>
+  )
+}
+
+export default User
