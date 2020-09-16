94f9514310c730b76a5334fac7cdf4e7748e3a4f
Shaun <shaun@thenetninja.co.uk>
2020-09-16 10:15:12 +0100

    lesson-7

--- src/skeletons/SkeletonArticle.js
+++ src/skeletons/SkeletonArticle.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Shimmer from './Shimmer'
 import SkeletonElement from './SkeletonElement'
 
 const SkeletonArticle = ({ theme }) => {
@@ -12,6 +13,7 @@ const SkeletonArticle = ({ theme }) => {
         <SkeletonElement type="text" />
         <SkeletonElement type="text" />
       </div>
+      <Shimmer />
     </div>
   )
 }
--- src/skeletons/SkeletonProfile.js
+++ src/skeletons/SkeletonProfile.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Shimmer from './Shimmer'
 import SkeletonElement from './SkeletonElement'
 
 const SkeletonProfile = ({ theme }) => {
@@ -16,6 +17,7 @@ const SkeletonProfile = ({ theme }) => {
           <SkeletonElement type="text" />
         </div>
       </div>
+      <Shimmer />
     </div>
   )
 }
